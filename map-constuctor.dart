d37a799c2c274efd136b47c2b4792fa04cd120eb
Md.Mahadi Hasan <hmdmahadi359@gmail.com>
2024-10-19 23:41:13 +0600

    map-constuctor added

---
+++ map-constuctor.dart
@@ -0,0 +1,10 @@
+void main() {
+  var student = new Map();
+  student['Name'] = 'md.mahadi';
+  student['age'] = 30;
+  student['city'] = 'Jamalpur';
+  student['country'] = 'Bangladesh';
+  student['Blood Group'] = 'A+';
+  print(student['Name']);
+  ;
+}
