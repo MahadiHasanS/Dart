d189a6a8bf30fb1c06b028b6434dbd234afdec60
Md.Mahadi Hasan <hmdmahadi359@gmail.com>
2024-10-19 23:12:12 +0600

    swich case added

---
+++ swich case.dart
@@ -0,0 +1,22 @@
+void main() {
+  var marks = 20;
+  switch (marks) {
+    case 80:
+      print('A+');
+      break;
+    case 70:
+      print('A');
+      break;
+    case 60:
+      print('A-');
+      break;
+    case 50:
+      print('C');
+      break;
+    case 40:
+      print("d");
+      break;
+    default:
+      print('Result is not found');
+  }
+}
